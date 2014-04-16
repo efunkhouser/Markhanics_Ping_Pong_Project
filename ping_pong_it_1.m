5df8c2a447a125fb13790955a8da5ba705bf1f41
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2014-04-16 15:14:42 -0400

    Magnus effect is wrong in some parameter somewhere

--- ping_pong_it_1.m
+++ ping_pong_it_1.m
@@ -12,7 +12,7 @@ A = 0.00502654824; %m^2
 Cd = 0.5;
 rho = 1.225; %kg/m^3
 mu = 0.6; %coeff. of friction between ball and table
-S = 0.000207
+S = 0.000207;
 
 theta = -pi/10; %launch angle in radians
 v0 = 10; %m/s
@@ -23,20 +23,9 @@ Initial = [0;0.2;(v0*cos(theta));(v0*sin(theta));0]; %x0 y0 vx0 vy0
 options = odeset('Events',@events);
 %FB stands for First Bounce
 [T1, FB] = ode45(@proj_derivs,Times,Initial,options);
-Initial2 = [FB(end,1);FB(end,2);FB(end,3);-1*FB(end,4);Omega];
 % Initial2 = [0.3739   -0.0000    1.5580    1.8161];
 %after ode stops, next call:
-[T2, SB] = ode45(@proj_derivs,Times,Initial2,options);
-disp(SB(:,5))
-plot(FB(:,1),FB(:,2))
-hold on;
-plot(SB(:,1),SB(:,2))
-X = [0, 2.74];
-Y = [0, 0];
-X2 = [1.37, 1.37];
-Y2 = [0, 0.1525];
-plot (X,Y)
-plot (X2, Y2)
+
 
 %COLLISION MODELING: ADDING SPIN
 % Torque * time of impact = Lf - L0
@@ -46,9 +35,22 @@ F_impact = -2 * m * FB(end,4) / t_impact; % 2mv / t = change in p over t
 friction = mu * F_impact * -1 * sign(FB(end,3)); % mu * N * -vxhat
 Torque = r_ball * friction; %r x F
 rxp = FB(end,1)*m*-1*FB(end,4); %r x p = range * m * vy
-Omega = (Torque * t_impact + L_0 - rxp) / ((2/3)*m*r_ball^2) %CALC R CROSS P
+Omega = (Torque * t_impact + L_0 - rxp) / ((2/3)*m*r_ball^2); %CALC R CROSS P
+
+Initial2 = [FB(end,1);FB(end,2);FB(end,3);-1*FB(end,4);Omega];
+[T2, SB] = ode45(@proj_derivs,Times,Initial2,options);
+disp(SB(:,5))
 
 
+plot(FB(:,1),FB(:,2))
+hold on;
+plot(SB(:,1),SB(:,2))
+X = [0, 2.74];
+Y = [0, 0];
+X2 = [1.37, 1.37];
+Y2 = [0, 0.1525];
+plot (X,Y)
+plot (X2, Y2)
 
     function [value,isterminal,direction] = events(t,PV)
         value = PV(2);
@@ -61,7 +63,7 @@ Omega = (Torque * t_impact + L_0 - rxp) / ((2/3)*m*r_ball^2) %CALC R CROSS P
         y = PV(2);
         vx = PV(3);
         vy = PV(4);
-        Omega = PV(5);
+        omega = PV(5);
         
         dxdt = vx;
         dydt = vy;
@@ -69,8 +71,8 @@ Omega = (Torque * t_impact + L_0 - rxp) / ((2/3)*m*r_ball^2) %CALC R CROSS P
         Vhat = [vx;vy] ./ norm([vx;vy]);
         
         Fd = -0.5 * rho * A * Cd * (norm([vx;vy]))^2 .* Vhat;
-        Fm_x = S*-1*Omega*vx;
-        Fm_y = S*Omega*vy;
+        Fm_x = S*-1*omega*vx;
+        Fm_y = S*omega*vy;
         dvxdt = Fd(1) / m + Fm_x/m;
         dvydt = -g + Fd(2) / m + Fm_y/m;
         derivs = [dxdt;dydt;dvxdt;dvydt; 0];
